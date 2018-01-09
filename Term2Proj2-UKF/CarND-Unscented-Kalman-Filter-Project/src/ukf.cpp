bce0e0a01e800cc5bc5487cd4a7b6e73c33dc927
YudaWang <yuda.wang.cs@gmail.com>
2018-01-09 01:11:17 -0800

    radar only 100

--- Term2Proj2-UKF/CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
+++ Term2Proj2-UKF/CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
@@ -36,7 +36,7 @@ UKF::UKF() {
   std_a_ = 100;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 3;
+  std_yawdd_ = 100;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
