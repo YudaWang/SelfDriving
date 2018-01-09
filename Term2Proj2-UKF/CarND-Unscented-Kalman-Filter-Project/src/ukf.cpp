1d7fd4a1e8c74be62d85d7c6f34588953495f595
YudaWang <yuda.wang.cs@gmail.com>
2018-01-09 01:20:12 -0800

    lidar50 radar1

--- Term2Proj2-UKF/CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
+++ Term2Proj2-UKF/CarND-Unscented-Kalman-Filter-Project/src/ukf.cpp
@@ -36,7 +36,7 @@ UKF::UKF() {
   std_a_ = 50;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 4;
+  std_yawdd_ = 1;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
