9505b3933fa65ce6f28b50beba6c2690afcbd859
YudaWang <yuda.wang.cs@gmail.com>
2018-02-06 22:36:50 -0800

    Term2Proj4

--- Term2Proj4-PID/CarND-PID-Control-Project/src/PID.cpp
+++ Term2Proj4-PID/CarND-PID-Control-Project/src/PID.cpp
@@ -15,9 +15,9 @@ PID::PID() {
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-	Kp = Kp;
-	Ki = Ki;
-	Kd = Kd;
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
