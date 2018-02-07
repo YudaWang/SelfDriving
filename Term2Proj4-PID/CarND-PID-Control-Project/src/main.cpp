bf5b093a199cd2f6980705a0b465cfa799c9a68d
YudaWang <yuda.wang.cs@gmail.com>
2018-02-06 23:13:27 -0800

    Term2Proj4

--- Term2Proj4-PID/CarND-PID-Control-Project/src/main.cpp
+++ Term2Proj4-PID/CarND-PID-Control-Project/src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.4, 0.00, 0.8);
+  pid.Init(0.5, 0.00, 0.8);
   std::cout << "Kp = " << pid.Kp << " Ki = " << pid.Ki << " Kd = " << pid.Kd << std::endl;
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
