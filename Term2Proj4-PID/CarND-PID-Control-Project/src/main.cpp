f83de1b3d5917c8f4c5a2b95f7f22d1e363966e4
YudaWang <yuda.wang.cs@gmail.com>
2018-02-07 07:16:04 -0800

    Term2Proj4

--- Term2Proj4-PID/CarND-PID-Control-Project/src/main.cpp
+++ Term2Proj4-PID/CarND-PID-Control-Project/src/main.cpp
@@ -63,7 +63,7 @@ int main()
           steer_value = atan(-pid.TotalError())/(M_PI/2);
 
           // DEBUG
-          std::cout << "Speed = " << speed << " Angle = " << angle << std::endl;
+          // std::cout << "Speed = " << speed << " Angle = " << angle << std::endl;
           std::cout << " errSum = " << pid.errorSum << " Total Error = " << pid.TotalError() << std::endl;
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
