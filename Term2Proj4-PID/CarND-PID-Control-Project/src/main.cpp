06d3d7b0098605fee9054ee20a6bfcbda8369f21
YudaWang <yuda.wang.cs@gmail.com>
2018-02-08 22:23:24 -0800

    Term2Proj4

--- Term2Proj4-PID/CarND-PID-Control-Project/src/main.cpp
+++ Term2Proj4-PID/CarND-PID-Control-Project/src/main.cpp
@@ -45,6 +45,8 @@ int main()
   // pidThtAng.Init(0,0,0);
 
 
+
+
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -72,6 +74,10 @@ int main()
           steer_value = atan(-pid.TotalError())/(M_PI/2);
           // pidThtSpd.UpdateError(speed-spdTarget);
           // pidThtAng.UpdateError(angle);
+          float tht_spd_P = 0;
+          // float tht_spd_D = 0;
+          float tht_ang_P = 0;
+          // float tht_ang_D = 0;
 
           // DEBUG
           // std::cout << "Speed = " << speed << " Angle = " << angle << std::endl;
@@ -80,7 +86,7 @@ int main()
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = spdTarget/100;// + pidThtSpd + pidThtAng;
+          msgJson["throttle"] = spdTarget/100 + tht_spd_P*(spdTarget-speed) - tht_ang_P*angle;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
