f93b8320a989f0c97a945ebd07b682127ab5dc7a
YudaWang <yuda.wang.cs@gmail.com>
2018-02-08 23:05:01 -0800

    Term2Proj4

--- Term2Proj4-PID/CarND-PID-Control-Project/src/main.cpp
+++ Term2Proj4-PID/CarND-PID-Control-Project/src/main.cpp
@@ -35,7 +35,7 @@ int main()
   PID pid;
   // TODO: Initialize the pid variable.
   // pid.Init(5  , 0.0005, 100);
-  pid.Init(2, 0.0001, 200);
+  pid.Init(1, 0.0001, 100);
   std::cout << "Kp = " << pid.Kp << " Ki = " << pid.Ki << " Kd = " << pid.Kd << std::endl;
 
   // PID pidThtSpd;
@@ -74,9 +74,9 @@ int main()
           steer_value = atan(-pid.TotalError())/(M_PI/2);
           // pidThtSpd.UpdateError(speed-spdTarget);
           // pidThtAng.UpdateError(angle);
-          float tht_spd_P = 0.5;
-          float tht_ang_P = 1;
-          float tht_cte_P = 100;
+          float tht_spd_P = 1;
+          float tht_ang_P = 0.1;
+          float tht_cte_P = 10;
 
           // DEBUG
           // std::cout << "Speed = " << speed << " Angle = " << angle << std::endl;
@@ -85,7 +85,8 @@ int main()
           std::cout << "speed = " << speed << " angle = "<<angle<< std::endl;
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = spdTarget/100+tht_spd_P*(spdTarget-speed)-tht_ang_P*angle-tht_cte_P*cte*cte;
+          msgJson["throttle"] = spdTarget/100 - tht_spd_P*(speed-spdTarget)
+                                - tht_ang_P*fabs(angle) - tht_cte_P*fabs(cte);
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
