b6313c4ee973f1803944551d73e0b8225fc8ae1b
YudaWang <yuda.wang.cs@gmail.com>
2018-02-13 21:10:25 -0800

    Term2Proj5

--- Term2Proj5-MPC/CarND-MPC-Project/src/main.cpp
+++ Term2Proj5-MPC/CarND-MPC-Project/src/main.cpp
@@ -152,8 +152,8 @@ int main() {
           // next_y_vals = {0, 0};
 
           for (int iL=0; iL<ptsx.size(); iL++){
-            double x = ptsx[i];
-            double y = ptsy[i];
+            double x = ptsx[iL];
+            double y = ptsy[iL];
             next_x_vals.push_back(x*cos(psi)+y*sin(psi)-px);
             next_y_vals.push_back(-x*sin(psi)+y*cos(psi)-py);
           }
