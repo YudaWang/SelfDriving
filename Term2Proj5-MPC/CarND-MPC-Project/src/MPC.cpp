80b2da83353f01a62a6a820082a1d58803e528b0
YudaWang <yuda.wang.cs@gmail.com>
2018-02-16 00:56:08 -0800

    Term2Proj5

--- Term2Proj5-MPC/CarND-MPC-Project/src/MPC.cpp
+++ Term2Proj5-MPC/CarND-MPC-Project/src/MPC.cpp
@@ -49,18 +49,18 @@ class FG_eval {
     fg[0] = 0;
     // The part of the cost based on the reference state.
     for (size_t t = 0; t < N; t++) {
-      fg[0] += 10*CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += 100*CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 100*CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 500*CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
     // Minimize the use of actuators.
     for (size_t t = 0; t < N - 1; t++) {
-      fg[0] += 100*CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 500*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t], 2);
     }
     // Minimize the value gap between sequential actuations.
     for (size_t t = 0; t < N - 2; t++) {
-      fg[0] += 100*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 500*CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += 10*CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
     // Initial constraints
