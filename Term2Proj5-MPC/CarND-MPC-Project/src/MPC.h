70eef5fe43ed93bacca32ebd2a71f4a7296ff2a8
YudaWang <yuda.wang.cs@gmail.com>
2018-02-17 07:30:28 -0800

    Term2Proj5

--- Term2Proj5-MPC/CarND-MPC-Project/src/MPC.h
+++ Term2Proj5-MPC/CarND-MPC-Project/src/MPC.h
@@ -30,7 +30,7 @@ class MPC {
 
   // Solve the model given an initial state and polynomial coefficients.
   // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  Solution Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
 
 #endif /* MPC_H */
