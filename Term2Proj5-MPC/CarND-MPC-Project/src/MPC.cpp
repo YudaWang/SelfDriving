aa9fdf2b4a3631c355169dc0bdc3d23ce32ab0c3
YudaWang <yuda.wang.cs@gmail.com>
2018-02-17 18:55:43 -0800

    Term2Proj5

--- Term2Proj5-MPC/CarND-MPC-Project/src/MPC.cpp
+++ Term2Proj5-MPC/CarND-MPC-Project/src/MPC.cpp
@@ -5,21 +5,6 @@
 
 using CppAD::AD;
 
-
-
-// This value assumes the model presented in the classroom is used.
-//
-// It was obtained by measuring the radius formed by running the vehicle in the
-// simulator around in a circle with a constant steering angle and velocity on a
-// flat terrain.
-//
-// Lf was tuned until the the radius formed by the simulating the model
-// presented in the classroom matched the previous radius.
-//
-// This is the length from front to CoG that has a similar radius.
-const double Lf = 2.67;
-double ref_v = 30;
-
 // The solver takes all the state variables and actuator
 // variables in a singular vector. Thus, we should to establish
 // when one variable starts and another ends to make our lifes easier.
--- Term2Proj5-MPC/CarND-MPC-Project/src/MPC.h
+++ Term2Proj5-MPC/CarND-MPC-Project/src/MPC.h
@@ -6,11 +6,27 @@
 
 using namespace std;
 
+
+// This value assumes the model presented in the classroom is used.
+//
+// It was obtained by measuring the radius formed by running the vehicle in the
+// simulator around in a circle with a constant steering angle and velocity on a
+// flat terrain.
+//
+// Lf was tuned until the the radius formed by the simulating the model
+// presented in the classroom matched the previous radius.
+//
+// This is the length from front to CoG that has a similar radius.
+const double Lf = 2.67;
+const double ref_v = 30;
+
 // TODO: Set the timestep length and duration
 const size_t N = 10;
 const double dt = 0.05; 
 // too large N, dt cause total MPC range out of way point range, so out-of-range part get terrrible fit
 // too littl N, dt cause no prediction at all
+
+
 
 struct Solution {
 	vector<double> x;
