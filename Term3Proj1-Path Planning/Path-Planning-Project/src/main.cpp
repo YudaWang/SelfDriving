e4279c2f114a0949da55b9e9488beaed8ecc1b47
YudaWang <yuda.wang.cs@gmail.com>
2018-07-21 15:30:43 -0700

    test

--- Term3Proj1-Path Planning/Path-Planning-Project/src/main.cpp
+++ Term3Proj1-Path Planning/Path-Planning-Project/src/main.cpp
@@ -250,6 +250,29 @@ int main() {
             int prev_size = previous_path_y.size();
             cout<<"previously didn't finished path size = "<<prev_size<<endl;/////////////////////////
 
+            if(prev_size>0){
+              car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            for(int i=0; i<sensor_fusion.size(); i++){
+              //if car in my lane
+              float d = sensor_fusion[i][6];
+              if(d<(2+4*lane+2) && d>(2+4*lane-2)){
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx+vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                check_car_s += prev_size*0.02*check_speed;
+
+                if(check_car_s-car_s>0 && check_car_s-car_s<30){ // if potential collision
+                  ref_vel = 29.5;
+                }
+              }
+            }
+
             // original sparse (x,y) waypoint, which will be splined later
             vector<double> ptsx;
             vector<double> ptsy;
